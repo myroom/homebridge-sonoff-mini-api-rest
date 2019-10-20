9fa710340ba615a0a3b63c7a1091caba5532ea06
Aleksandr Usoltsev <boss@myroom.ru>
2019-10-21 02:36:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# homebridge-sonoff-mini-api-rest
---
+++ index.js
@@ -0,0 +1,84 @@
+var superagent = require("superagent");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  //console.log("homebridge API version: " + homebridge.version);
+
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  
+  homebridge.registerAccessory("homebridge-sonoff-mini-api-rest", "Sonoff", SonoffAccessory);
+}
+
+function SonoffAccessory(log, config) {
+  console.log("Sonoff Accessory Init");
+
+  this.log = log;
+  this.name = config["name"];
+  this.id = config["id"];
+  this.uri = config["uri"];
+  this.debug = config.debug || false;
+
+  this.service = new Service.Lightbulb(this.name);
+  
+  this.service
+    .getCharacteristic(Characteristic.On)
+    .on('get', this.getState.bind(this))
+    .on('set', this.setState.bind(this)); 
+}
+
+SonoffAccessory.prototype.getState = function(callback) {
+  this.log("Getting current state...");
+
+  superagent
+  .post(this.uri+'/zeroconf/info')
+  .send({ "deviceid": this.id, "data": { } }) // sends a JSON post body
+  .set('X-API-Key', 'foobar')
+  .set('accept', 'json')
+  .end((error, response) => {
+    if (!error && response.statusCode == 200) {
+      var json = JSON.parse(response.text).data;
+      var state = JSON.parse(json).switch;
+
+      if (this.debug)           
+          this.log('getState() request returned successfully ('+response.statusCode+'). Body: '+JSON.stringify(json));
+
+      this.log("Sonoff state is %s", state);
+
+      var on = (state == "on") ? true : false;
+      callback(null, on); 
+    }
+    else {
+      this.log("Function getState(). Error getting state (status code %s): %s", response.statusCode, error.message);
+      callback(error);
+    }
+  });
+}
+
+  
+SonoffAccessory.prototype.setState = function(state, callback) {  
+  
+  var SonoffState = (state == true) ? "on" : "off";
+  this.log("Set state to %s", SonoffState);
+
+  superagent
+    .post(this.uri+'/zeroconf/switch')
+    .send({ "deviceid": this.id, "data": { "switch": SonoffState } }) // sends a JSON post body
+    .set('X-API-Key', 'foobar')
+    .set('accept', 'json')
+    .end((error, response) => {
+      if (!error && response.statusCode == 200) {
+        if (this.debug)           
+            this.log('setState() request returned successfully ('+response.statusCode+'). Body: '+JSON.stringify(response));
+        callback(null, state);
+      }
+      else {
+        this.log("Function setState(). Error getting state (status code %s): %s", response.statusCode, error.message);
+        callback(error);
+      }      
+    });
+}
+
+SonoffAccessory.prototype.getServices = function() {
+  return [this.service];
+}
